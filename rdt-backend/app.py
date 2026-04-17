a016606a252c7ff8510bf6b62203e83986ea1f30
Jessica Wang <jessiwang2000@gmail.com>
2026-04-16 19:46:19 -0700

    update CORS headers

--- rdt-backend/app.py
+++ rdt-backend/app.py
@@ -29,7 +29,7 @@ def _check_admin(req):
 @app.after_request
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Admin-Token"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 
